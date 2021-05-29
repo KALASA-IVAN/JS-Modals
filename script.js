bafbacfe2ef71f51f64cf79e9e2b0dc10536b39e
KALASA-IVAN <moikalasa12@gmail.com>
2021-05-29 13:32:24 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnsCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++){
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+btnsCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function(e){
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  } else if (e.key === 'Enter' && modal.classList.contains('hidden')) {
+    openModal();
+  }
+})
